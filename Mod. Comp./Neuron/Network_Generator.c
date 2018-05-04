492b1516d464d89b1b5f64dea1f91263ed9919d3
Lucas Saavedra Vaz <lucassvaz@yahoo.com.br>
2018-05-04 15:07:19 -0300

    Update Neuron Project

--- Mod. Comp./Neuron/Network_Generator.c
+++ Mod. Comp./Neuron/Network_Generator.c
@@ -7,10 +7,12 @@ int main()
   FILE* file_regular = fopen("G_Regular.dat", "w");
   FILE* file_random = fopen("G_Random.dat", "w");
   FILE* file_barabasi = fopen("G_Barabasi.dat", "w");
-  int i,j, Regular[500][500], Random[500][500], Barabasi[500][500], *Barabasi_Edges, nEdges_Nodes=2, a, b;
+  int i,j, Regular[500][500], Random[500][500], Barabasi[500][500], *Barabasi_Edges, nEdges_Nodes=2, a;
 
   srand((unsigned)time(NULL));
 
+  //======================================================================================================================
+  
   for (i=0;i<500;i++){
     for (j=0;j<500;j++){
       Regular[i][j]=0;
@@ -19,6 +21,8 @@ int main()
     }
   }
 
+  //====================================================================================================================== REGULAR
+  
   for (i=0;i<500;i++){
     Regular[i][(i+1)%500]=1;
     Regular[(i+1)%500][i]=1;
@@ -31,15 +35,14 @@ int main()
     fprintf(file_regular,"\n");
   }
 
-  for (i=0;i<499;i++){
-    a = rand()%500;
-    b = rand()%500;
-    while (Random[a][b]!=0){
+  //====================================================================================================================== RANDOM
+  
+  for (i=0;i<500;i++){
+    do{
       a = rand()%500;
-      b = rand()%500;
-    }
-    Random[a][b]=1;
-    Random[b][a]=1;
+    } while (Random[i][a]!=0 && a==i);
+    Random[a][i]=1;
+    Random[i][a]=1;
   }
 
   for (i=0;i<500;i++){
@@ -48,7 +51,38 @@ int main()
     }
     fprintf(file_random,"\n");
   }
+  
+  //====================================================================================================================== BARABASI
+  
+  Barabasi_Edges = (int *)calloc(nEdges_Nodes,sizeof(int));
+  do{
+    a = rand()%500;
+  }while(a==0);
+  Barabasi[a][0]=1;
+  Barabasi[0][a]=1;
+  Barabasi_Edges[0]=a;
+  Barabasi_Edges[1]=0;
+  
+  for (i=1;i<500;i++){
+    do{
+      a = Barabasi_Edges[rand()%nEdges_Nodes];
+    } while (Barabasi[a][i]!=0 && a==i);
+    Barabasi[a][i]=1;
+    Barabasi[i][a]=1;
+    nEdges_Nodes+=2;
+    Barabasi_Edges = (int *)realloc(Barabasi_Edges,nEdges_Nodes*sizeof(int));
+    Barabasi_Edges[nEdges_Nodes-1]=i;
+    Barabasi_Edges[nEdges_Nodes-2]=i;
+  }
 
+  for (i=0;i<500;i++){
+    for (j=0;j<500;j++){
+      fprintf(file_barabasi, "%d ",Barabasi[i][j]);
+    }
+    fprintf(file_barabasi,"\n");
+  }
+
+  //======================================================================================================================
 
   fclose(file_regular);
   fclose(file_random);
--- Mod. Comp./Neuron/Neuron.c
+++ Mod. Comp./Neuron/Neuron.c
@@ -2,17 +2,39 @@
 #include <math.h>
 #include <stdlib.h>
 
+typedef struct {
+	float x,y,S,act;
+}Neuron;
+
 #define FILE_NAME "plot.dat"
 #define DELTA_T 0.01
 
 int main()
 {
-  FILE* file_ptr = fopen(FILE_NAME, "w");
+  FILE* file_output = fopen(FILE_NAME, "w");
+  FILE* file_input = fopen("G_Regular.dat", "r");
   float Time=0;
-  int i, ;
+  int i, j, Matrix[500][500];
+  Neuron *Network = (Neuron *)calloc(500*sizeof(Neuron));
   char CallPlot[1000];
 
-  fprintf(file_ptr,"#                                           Neuron                                          #\n");
+  //float x = ((float)rand()/(float)(RAND_MAX)) * a;
+
+  for(i=0; i < 500; i++){
+    for(j = 0; j < 500; j++) {
+    	Matrix[i][j]=0;
+    	if (!fscanf(file_input, "%d", &Matrix[i][j])) 
+    	    break; 
+    	}
+  }
+
+  fprintf(file_output,"#                                           Neuron                                          #\n");
+
+  for (i=0;i<500;i++){
+  	Network[i].x=(((float)rand()/(float)(RAND_MAX)) * 4)-2;
+  	Network[i].y=((float)rand()/(float)(RAND_MAX)) * 4;
+  	Network[i].act=0.2;
+  }
 
   while (Time<=1000){
 
@@ -20,10 +42,10 @@ int main()
   }
   
 
-  snprintf(CallPlot,sizeof(CallPlot),"gnuplot -p -e \"set terminal x11 enhanced background rgb \'grey\';set key outside; set key center top;set title \'Population\';set xlabel \'Time\';set ylabel \'Population\'; set logscale y; set yrange [0.1:1000];plot \'plot.dat\' using 1:2 title \'Grass\' with line lt -1 lw 2 lc rgb \'green\', \\\n \'plot.dat\' using 1:3 title \'Rabbit\' with line lt -1 lw 2 lc rgb \'brown\', \\\n \'plot.dat\' using 1:4 title \'Insect\' with line lt -1 lw 2 lc rgb \'black\', \\\n \'plot.dat\' using 1:5 title \'Lizard\' with line lt -1 lw 2 lc rgb \'orange\', \\\n \'plot.dat\' using 1:6 title \'Cobra\' with line lt -1 lw 2 lc rgb \'blue\', \\\n \'plot.dat\' using 1:7 title \'Bird\' with line lt -1 lw 2 lc rgb \'yellow\'\"");
+  /*snprintf(CallPlot,sizeof(CallPlot),"gnuplot -p -e \"set terminal x11 enhanced background rgb \'grey\';set key outside; set key center top;set title \'Population\';set xlabel \'Time\';set ylabel \'Population\'; set logscale y; set yrange [0.1:1000];plot \'plot.dat\' using 1:2 title \'Grass\' with line lt -1 lw 2 lc rgb \'green\', \\\n \'plot.dat\' using 1:3 title \'Rabbit\' with line lt -1 lw 2 lc rgb \'brown\', \\\n \'plot.dat\' using 1:4 title \'Insect\' with line lt -1 lw 2 lc rgb \'black\', \\\n \'plot.dat\' using 1:5 title \'Lizard\' with line lt -1 lw 2 lc rgb \'orange\', \\\n \'plot.dat\' using 1:6 title \'Cobra\' with line lt -1 lw 2 lc rgb \'blue\', \\\n \'plot.dat\' using 1:7 title \'Bird\' with line lt -1 lw 2 lc rgb \'yellow\'\"");
 
   system(CallPlot);
-  printf("Done!\n");
-  fclose(file_ptr);
+  printf("Done!\n");*/
+  fclose(file_output);
   return 0;
 }
