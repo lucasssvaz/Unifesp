924a4c61832e7a4e50ba23fa253e59b5f71d5e04
Lucas Saavedra Vaz <32426024+lucasssvaz@users.noreply.github.com>
2018-09-21 15:33:14 -0300

    Add files via upload

---
+++ AED2/TG1/BubbleSort.py
@@ -0,0 +1,21 @@
+import pandas
+
+def BubbleSort(data):
+    nRows = data.shape[0]
+    nColumns = data.shape[1]
+    k=0
+    
+    for i in range(nRows):
+        for j in range(1,nElements-i):
+            while True:
+                if data.loc[j-1,k]>dataloc[]
+            
+    
+        
+
+
+files = {'1':'credit.csv','2':'eeg.csv','3':'kc1.csv','4':'phoneme.csv'}
+
+dataset = files[input("Select the dataset:\n1) Credit \n2) Eeg-eye \n3) KC1 \n4) Phoneme \n")]
+
+data = pandas.read_csv(dataset)
