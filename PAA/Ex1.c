b70caf03676e23830e05b3e211fcaf2a9518e969
Lucas Vaz <lucassvaz@yahoo.com.br>
2019-09-19 11:42:14 -0300

    Updating Submodules

--- PAA/Ex1.c
+++ PAA/Ex1.c
@@ -1,53 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define DELTA 1
+
+int Verify_Frontiers(int *Chords, int Size_Ch, int *Frontier, int Size_Sc){
+
+	int i;
+	int *Count = (int *) calloc(Size_Ch,sizeof(int));
+
+	for (i = 0; i < Size_Ch; i++){
+        if (i == 0){
+            if (Chords[i] >= Frontier[i] && Chords[i] < Frontier[i+1]){ //Initial Frontier
+                Count[i]++;
+            } else if (Chords[i]+Size_Sc >= Frontier[Size_Ch-1] && Chords[i]+Size_Sc < (Frontier[0]+Size_Sc)){ //New Frontier
+                Count[Size_Ch-1]++;
+            }
+        } else if (i == Size_Ch-1){
+            if ((Chords[i] >= Frontier[i] && Chords[i] < (Frontier[0]+Size_Sc))){ //Initial Frontier
+                Count[i]++;
+            } else if ((Chords[i] >= Frontier[i-1] && Chords[i] < (Frontier[i]))){ //New Frontier
+                Count[i-1]++;
+            }
+        } else {
+            if (Chords[i] >= Frontier[i] && Chords[i] < Frontier[i+1]){ //Initial Frontier
+                Count[i]++;
+            } else if (Chords[i] >= Frontier[i-1] && Chords[i] < Frontier[i]){ //New Frontier
+                Count[i-1]++;
+            }
+        }
+    }
+
+	for (i = 0; i < Size_Ch; i++){
+		if (Count[i] != 1)
+			return 0;
+	}
+
+	return 1;
+
+}
+
+
 int main (){
 
 	int i;
 	int Size_Ch, Size_Sc, *Chords;
-	int Res = 1, F_Size;
-	int Dist_D, Dist_E;
+	int Res = 0, Inc;
+	int *Frontier;
 
 	scanf("%d %d",&Size_Sc,&Size_Ch);
 
 	Chords = (int *) calloc(Size_Ch, sizeof(int));
+	Frontier = (int *) calloc(Size_Ch, sizeof(int));
 
 	for(i = 0; i < Size_Ch; i++){
 		scanf("%d",&Chords[i]);
 	}
 
-	F_Size = Size_Sc/Size_Ch;
+	Inc = Size_Sc/Size_Ch;
 
-	//Dist_E >= F_Size - Dist_D
+	for(i = 1; i < Size_Ch; i++){
+		Frontier[i] = (Frontier[i-1] + Inc) % Size_Sc;
+	}
 
-	for (i = 0; i<Size_Ch; i++){
-		if (i == Size_Ch-1){
-			Dist_E = Chords[i] - Chords[i-1];
-			Dist_D = (Chords[0] + Size_Sc) - Chords[i];
-			if (Dist_E + Dist_D <= F_Size){
-				Res = 0;
-				break;
-			}
-		} else if (i == 0){
-			Dist_E = (Chords[i]+Size_Sc)-Chords[Size_Ch-1];
-			Dist_D = Chords[i+1]-Chords[i];
-			if (Dist_E + Dist_D <= F_Size){
-				Res = 0;
-				break;
-			}
-		} else {
-			Dist_E = Chords[i]-Chords[i-1];
-			Dist_D = Chords[i+1]-Chords[i];
-			if (Dist_E + Dist_D <= F_Size){
-				Res = 0;
-				break;
-			}
+
+	int Max_D = Inc;
+
+	while (Frontier[0] <= Max_D){
+
+		if ((Res = Verify_Frontiers(Chords, Size_Ch, Frontier, Size_Sc)))
+			break;
+
+		for(i = 0; i < Size_Ch; i++){
+			Frontier[i] = (Frontier[i] + DELTA) % Size_Sc;
 		}
 
 	}
 
+
+
 	Res ? printf("S\n") : printf("N\n");
 	free (Chords);
+	free (Frontier);
 
 	return 0;
 }
